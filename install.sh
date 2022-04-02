e4fe0199ba39d44bb1fd379f1040f3147b65b59d
hinadome <hinadome@gmail.com>
2022-04-01 19:58:38 -0700

    backend and template install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install dependencies
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
+source ~/.bashrc;
+nvm --version;
+nvm install 16; # or install the latest version with `nvm install node`
+nvm use 16;
+node --version;
+npm install --global yarn
+npm i -g pm2
+cd ./frontend && yarn install && yarn build;
+cd ../backend && yarn install;
